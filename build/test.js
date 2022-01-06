200c7bdb995409f16f350fde76ad4331defa75aa
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2022-01-05 22:40:56 -0300

    fix tests

--- build/test.js
+++ build/test.js
@@ -2,9 +2,9 @@ import { OpenPixConnection } from "./connection/OpenPixConnection.js";
 var a = new OpenPixConnection("", "tests");
 
 async function test() {
-  let b = await a.getCharge("1");
+  let b = await a.getCharge(1);
   console.log(b);
-  let c = await a.getCharge("1");
+  let c = await a.getCharge(1);
   console.log(c);
 }
 
