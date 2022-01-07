086fb99b083b227162d314c0325a787c5daec635
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2022-01-07 08:56:17 -0300

    feat: mocks implementation

---
+++ src/mocks/genericMocks.js
@@ -0,0 +1,30 @@
+const genericMocks = {
+    "charge": {
+        "status": "ACTIVE",
+        "customer": {},
+        "value": 100,
+        "comment": "good",
+        "correlationID": "9134e286-6f71-427a-bf00-241681624586",
+        "paymentLinkID": "7777-6f71-427a-bf00-241681624586",
+        "paymentLinkUrl": "https://openpix.com.br/pay/9134e286-6f71-427a-bf00-241681624586",
+        "globalID": "Q2hhcmdlOjcxOTFmMWIwMjA0NmJmNWY1M2RjZmEwYg==",
+        "qrCodeImage": "https://api.openpix.dev/openpix/charge/brcode/image/9134e286-6f71-427a-bf00-241681624586.png",
+        "brCode": "000201010212261060014br.gov.bcb.pix2584http://localhost:5001/openpix/testing?transactionID=867ba5173c734202ac659721306b38c952040000530398654040.015802BR5909LOCALHOST6009Sao Paulo62360532867ba5173c734202ac659721306b38c963044BCA",
+        "additionalInfo": [
+            {
+            "key": "Product",
+            "value": "Pencil"
+            },
+            {
+            "key": "Invoice",
+            "value": "18476"
+            },
+            {
+            "key": "Order",
+            "value": "302"
+            }
+        ],
+        "createdAt": "2021-03-02T17:28:51.882Z",
+        "updatedAt": "2021-03-02T17:28:51.882Z"
+    }
+}; 
