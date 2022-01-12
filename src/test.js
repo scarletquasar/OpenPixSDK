54acd43ff00a7375c19d9ecb0fd89cbb9817c64f
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2022-01-12 20:15:23 -0300

    feat: confirmPayment function

--- src/test.js
+++ src/test.js
@@ -3,12 +3,7 @@ import { ConnectionType } from "./models/enums/ConnectionType.js";
 
 var a = new OpenPixConnection("", ConnectionType.mockTests);
 async function test() {
-    var b = await a.startPayment({
-        "pixKey": "c4249323-b4ca-43f2-8139-784baab09b93",
-        "pixKeyType": "RANDOM",
-        "value": 1,
-        "correlationID": "pay1"
-        });
+    var b = await a.confirmPayment({correlationID: 12342});
     console.log(b);
 }
 
