3893b14044756904bae6cb78cda9a1d14e58656f
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2022-01-15 10:50:55 -0300

    chore: PixWebhook model fixed informations

--- src/models/pix/PixWebhook.js
+++ src/models/pix/PixWebhook.js
@@ -1,10 +1,13 @@
 import { ConvertibleObject } from "../extensions/ConvertibleObject.js";
 
 class PixWebhook extends ConvertibleObject {
+    id = null;
     name = null;
     url = null;
     authorization = null;
     isActive = null;
+    createdAt = null;
+    updatedAt = null;
 
     constructor(raw = []) {
         super();
