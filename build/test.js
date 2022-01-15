06e309defdca97d6b914098ac763d9a968d59caa
Kayky Vitor Cruz <70824102+EternalQuasar0206@users.noreply.github.com>
2022-01-14 21:17:19 -0300

    fix: fixed data result variable names

--- build/test.js
+++ build/test.js
@@ -3,7 +3,9 @@ import { ConnectionType } from "./models/enums/ConnectionType.js";
 var a = new OpenPixConnection("", ConnectionType.mockTests);
 
 async function test() {
-  var b = await a.createCustomer();
+  var b = await a.confirmPayment({
+    correlationID: 2
+  });
   console.log(b);
 }
 
